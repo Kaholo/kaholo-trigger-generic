c1ef10a6b01d3fd9890d2c3d2b96f5884ee437b1
Daniel Grad <daniel.g@kaholo.io>
2022-05-16 12:24:01 +0200

    [KP-680] Generic Trigger

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-function hello(name) {
-  const greeting = `hello ${name}`;
-  return greeting;
+function extractRequestData(req) {
+  let data = null;
+
+  switch (req.headers["content-type"]) {
+    case "application/json":
+    case "application/x-www-form-urlencoded":
+      data = req.body;
+      break;
+
+    default:
+      throw new Error(`Unsupported 'content-type' header. Received: ${req.headers["content-type"]}`);
+  }
+
+  return data;
+}
+
+async function defaultWebhook(req, res, settings, triggerControllers) {
+  try {
+    const { webhookName } = req.params;
+
+    const data = extractRequestData(req);
+    const executionMessage = `generic trigger ${webhookName}`;
+
+    triggerControllers.forEach((trigger) => {
+      if (trigger.params.webhookName !== webhookName) {
+        return;
+      }
+
+      trigger.execute(executionMessage, data);
+    });
+
+    res.status(200).send("OK");
+  } catch (error) {
+    res.status(422).send(error.message);
+  }
+
+  return Promise.resolve();
 }
 
 module.exports = {
-  hello,
+  defaultWebhook,
 };
