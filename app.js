9930b7b6078591fb21b7e610069ab65b48a40033
graddaniel <48184737+graddaniel@users.noreply.github.com>
2022-05-25 11:53:26 +0200

    [KP-680] Generic Trigger (#1)

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
-function hello(name) {
-  const greeting = `hello ${name}`;
-  return greeting;
+const PAYLOAD_HTTP_METHODS = [
+  "PUT",
+  "POST",
+  "PATCH",
+];
+
+async function defaultWebhook(req, res, settings, triggerControllers) {
+  try {
+    const { webhookName } = req.params;
+    const httpMethod = req.method;
+
+    const data = extractRequestData(req, httpMethod);
+    const executionMessage = `generic trigger ${webhookName}`;
+
+    triggerControllers.forEach((trigger) => {
+      if (
+        trigger.params.httpMethod !== "any"
+          && (trigger.params.webhookName !== webhookName
+          || trigger.params.httpMethod !== httpMethod)
+      ) {
+        return;
+      }
+
+      trigger.execute(executionMessage, data);
+    });
+
+    res.status(200).send("OK");
+  } catch (error) {
+    res.status(422).send(error.message);
+  }
+
+  return Promise.resolve();
+}
+
+function extractRequestData(req, httpMethod) {
+  if (PAYLOAD_HTTP_METHODS.includes(httpMethod)) {
+    return extractDataFromReqBody(req);
+  }
+  return req.query;
+}
+
+function extractDataFromReqBody(req) {
+  switch (req.headers["content-type"]) {
+    case "application/json":
+    case "application/x-www-form-urlencoded":
+      return req.body;
+
+    default:
+      throw new Error(`Unsupported 'content-type' header. Received: ${req.headers["content-type"]}`);
+  }
 }
 
 module.exports = {
-  hello,
+  defaultWebhook,
 };
